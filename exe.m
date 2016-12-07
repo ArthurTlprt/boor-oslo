997f8ee065a9ac60abe17fe04e9cdcc4099ea3af
ArthurTlprt <arthur.talpaert@isen-lille.fr>
2016-12-07 18:18:30 +0100

    fonctions qui affichent un graphes faux

--- exe.m
+++ exe.m
@@ -1,15 +1,22 @@
-m = 3
-k = m+1
+m = 3;
+k = m+1;
 d = [ 1, 1;
+      1, 2
       2, 2;
-      3, 2;
-      4, 1
+      2, 1;
     ];
 p = length(d);
-t = 1:0.05:10
-S = 0
-for i=1:p
-   S = d(i)*N(t)
 
+vecteur_noeud = 1:p+k+1;
+interval = 1:0.5:10;
+S = zeros(length(interval), 2);
+for i=1:length(interval)
+  for j=1:p
+     S(i, 1) = S(i, 1) + d(j)*fonctionN(vecteur_noeud, interval(i), m, j);
+     S(i, 2) = S(i, 1) + d(j)*fonctionN(vecteur_noeud, interval(i), m, j);
+  end
+end
 
-% N(vecteur_noeud, t, m, i)
+plot(S(:, 1), S(:, 2))
+pause;
+% fonctionN(vecteur_noeud, t, m, i)
